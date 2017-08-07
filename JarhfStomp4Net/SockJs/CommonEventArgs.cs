88404139540fe09acc4ac9bd1336e130b43905be
Fadi Kahhaleh <fadi.kahhaleh@motorolasolutions.com>
2017-08-07 14:20:34 -0500

    Refactored code to allow for different websocket implementations. (currently WebSocket4Net and WebSocketSharp are implemented).
    Using Global Project PreProcessor Def:
    WEBSOCKET4NET
    or
    WEBSOCKETSHARP
    to choose between which implementation gets compiled.

---
+++ JarhfStomp4Net/SockJs/CommonEventArgs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarhfStomp4Net.SockJs
+{
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        public MessageReceivedEventArgs(string msgBody)
+        {
+            this.Message = msgBody;
+        }
+        public string Message { get; internal set; }
+    }
+    public class ClosedEventArgs : EventArgs
+    {
+        internal ClosedEventArgs(ushort code, string reason, bool cleanlyClosed)
+        {
+            this.Code = code;
+            this.Reason = reason;
+            this.WasClean = cleanlyClosed;
+        }
+        public ushort Code { get; }
+        public string Reason { get; }
+        public bool WasClean { get; }
+    }
+    public class ErrorEventArgs : EventArgs
+    {
+        internal ErrorEventArgs(Exception ex, string errDetails)
+        {
+            this.Exception = ex;
+            this.Message = errDetails;
+        }
+        public Exception Exception { get; }
+        public string Message { get; }
+    }
+}
